c203a4a43c50163a7ae79db321b52d7a1539a37a
Davi161108 <davidavidaguiarfonseca@gmail.com>
2026-05-25 17:29:11 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 
 import Logo from "./components/Logo";
 import Dashboard from "./pages/Dashboard";
+import Footer from "./componentes/Footer";
+import FomularioCadastro from "./componentes/FomularioCadastro";
+import ListaUser from "./componentes/ListaUser"; // Importado como ListaUser
 
 function App() {
+  // Array inicial de estados
+  const [email, setEmail] = useState([]);
+
+  // Função que recebe o novo usuário e adiciona no array
+  function adicionarEmail(novoEmail) {
+    setEmail([...email, novoEmail]);
+  }
+  
   const [mostrarIntro, setMostrarIntro] = useState(true);
 
   const terminouAnimacao = () => {
@@ -29,6 +40,19 @@ function App() {
       >
         <Logo onComplete={terminouAnimacao} />
       </div>
+
+      <div className="container">
+
+      {/* 1. Formulário de Cadastro */}
+      <FomularioCadastro adicionarEmail={adicionarEmail} />
+
+      {/* 2. Lista de Usuários (CORRIGIDO: usando o nome do import e passando a prop 'user') */}
+      <ListaUser email={email} />
+
+      {/* 3. Rodapé sempre no final */}
+      <Footer />
+
+    </div>
     );
   }
 
