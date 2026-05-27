e5f69799518417b840e4c2d67da2cf930eb307b5
Davi161108 <davidavidaguiarfonseca@gmail.com>
2026-05-27 12:51:33 -0300

    Update CardOrcamento.jsx

--- src/components/CardOrcamento.jsx
+++ src/components/CardOrcamento.jsx
@@ -1,46 +1,46 @@
 import {
-  Card,
-  CardContent,
-  Typography,
-  LinearProgress,
+    Card,
+    CardContent,
+    Typography,
+    LinearProgress,
 } from "@mui/material";
 
 import GraficoPizza from "./GraficoPizza";
 
 export default function CardOrcamento() {
-  return (
-    <Card
-      sx={{
-        background: "#161616",
-        color: "#fff",
-        borderRadius: 4,
-      }}
-    >
-      <CardContent>
-        <GraficoPizza />
+    return (
+        <Card
+            sx={{
+                background: "#161616",
+                color: "#fff",
+                borderRadius: 4,
+            }}
+        >
+            <CardContent>
+                <GraficoPizza />
 
-        <Typography sx={{ fontSize: 12, color: "#a0aab0", mt: 1 }}>
-          ORÇAMENTO VARIÁVEL
-        </Typography>
+                <Typography sx={{ fontSize: 12, color: "#a0aab0", mt: 1 }}>
+                    ORÇAMENTO VARIÁVEL
+                </Typography>
 
-        <Typography sx={{ fontWeight: "bold" }}>
-          R$ 1.500 / R$ 5.000
-        </Typography>
+                <Typography sx={{ fontWeight: "bold" }}>
+                    R$ 1.500 / R$ 5.000
+                </Typography>
 
-        <LinearProgress
-          variant="determinate"
-          value={30}
-          sx={{
-            height: 8,
-            borderRadius: 5,
-            backgroundColor: "#232d2d",
-            mt: 1,
-            "& .MuiLinearProgress-bar": {
-              backgroundColor: "#47a899",
-            },
-          }}
-        />
-      </CardContent>
-    </Card>
-  );
+                <LinearProgress
+                    variant="determinate"
+                    value={30}
+                    sx={{
+                        height: 8,
+                        borderRadius: 5,
+                        backgroundColor: "#232d2d",
+                        mt: 1,
+                        "& .MuiLinearProgress-bar": {
+                            backgroundColor: "#47a899",
+                        },
+                    }}
+                />
+            </CardContent>
+        </Card>
+    );
 }
